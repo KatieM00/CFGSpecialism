32101c0efd7bdd08c713ca43be08c8ea52ba4c94
katie <katiemroberts00@gmail.com>
2023-08-01 19:07:36 +0100

    Questions a,b,c

---
+++ Q5_Titanic.py
@@ -0,0 +1,15 @@
+import pandas as pd
+titanic = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv')
+
+# a. How many columns and rows does the data have?
+num_rows, num_cols = titanic.shape
+print(f"Number of rows: {num_rows}")
+print(f"Number of columns: {num_cols}")
+
+# b. Get a sense of data and find the min, max, and count/mean depending on the data type.
+data_summary = titanic.describe(include='all')
+print(data_summary)
+
+# c. Give an overview of all missing values in the data.
+missing_values = titanic.isnull().sum()
+print(missing_values)
